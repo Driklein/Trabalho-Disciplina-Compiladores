1842b8d5373fd1a2ce9b0278f7cae3db453918f7
Rodrigo Klein <79477834+Driklein@users.noreply.github.com>
2023-03-30 16:48:17 -0300

    Analisador Léxico

---
+++ T1 - Analisador Lexico/tokens.h
@@ -0,0 +1,25 @@
+
+#define INT 260
+#define DOUBLE 261
+#define FLOAT 262
+#define CHAR 263
+#define QUOTE 264
+#define DQUOTE 265
+#define LE 266
+#define GE 267
+#define EQ 268
+#define NE 269
+#define AND 270
+#define OR 271
+#define NOT 272
+#define IF 273
+#define THEN 274
+#define ELSE 275
+#define WHILE 276
+#define MENOR 278
+#define MAIOR 279
+#define IGUAL 280
+
+#define IDF 300
+#define INT_LIT 301
+#define F_LIT 302
