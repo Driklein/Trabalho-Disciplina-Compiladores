c760e851ba6f2df75d1167c1e4c36e955ad17506
Driklein <rodrigosiverisklein@gmail.com>
2023-03-31 20:09:14 -0300

    regras de atribuicao

--- T1 - Analisador Lexico/tokens.h
+++ T1 - Analisador Lexico/tokens.h
@@ -3,22 +3,9 @@
 #define DOUBLE 261
 #define FLOAT 262
 #define CHAR 263
-#define QUOTE 264
-#define DQUOTE 265
-#define LE 266
-#define GE 267
-#define EQ 268
-#define NE 269
-#define AND 270
-#define OR 271
-#define NOT 272
-#define IF 273
-#define THEN 274
-#define ELSE 275
-#define WHILE 276
-#define MENOR 278
-#define MAIOR 279
-#define IGUAL 280
+#define TIPO_VARIAVEL 264
+#define ABRE_CHAVE 265
+#define FECHA_CHAVE 266
 
 #define IDF 300
 #define INT_LIT 301
